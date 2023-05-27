ad3683cf5adef705984e5df2d1550c7175d40829
judeiwuji <judeiwuji@gmail.com>
2023-05-27 10:37:23 +0100

    update

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,5 @@
 import sha1 from 'sha1';
-import { request, response } from 'express';
+// import { request, response } from 'express';
 import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
