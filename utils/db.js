d001e93121493aafeccf658f233d71abddafa2b5
judeiwuji <judeiwuji@gmail.com>
2023-05-27 10:21:26 +0100

    Fixed: lint errors

--- utils/db.js
+++ utils/db.js
@@ -1,9 +1,10 @@
 import MongoClient from 'mongodb/lib/mongo_client';
+
 class DBClient {
   constructor() {
-    const host = process.env['DB_HOST'] || 'localhost';
-    const port = process.env['DB_PORT'] || 27017;
-    this.dbName = process.env['DB_DATABASE'] || 'files_manager';
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    this.dbName = process.env.DB_DATABASE || 'files_manager';
     this.client = new MongoClient(`mongodb://${host}:${port}/${this.dbName}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -11,7 +12,7 @@ class DBClient {
 
     this.client
       .connect()
-      .then((client) => {})
+      .then(() => {})
       .catch((err) => console.log(err));
   }
 
